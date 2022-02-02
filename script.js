defa225ffe60b0f8326fdd46411a858b94d61a82
mathilde <math68n8@stud.kea.dk>
2022-02-02 17:43:06 +0100

    added working filters, fixed styling, and bug fixes, did wayy to much in one commit

--- script.js
+++ script.js
@@ -1,33 +1,63 @@
 
 let actors;
+let filter="all";
 
-const jsonfile = "actors.json";
 
-async function getData(jsonfile) {
-  const jsonData = await fetch(jsonfile);
-    actors = await jsonData.json();
 
-  vis(actors);
-  console.log(actors);
+const fil ="./jsondata/actors.json";
+
+
+async function hentData() {
+    const result = await fetch(fil);
+  actors = await result.json();
+
+    vis(actors);
 }
 
-getData(jsonfile);
+
+
+
+document.addEventListener("DOMContentLoaded", start)
+function start() {
+    const filterKnapper =document.querySelectorAll("nav #button");
+   filterKnapper.forEach(knap => knap.addEventListener("click", filtrer)); 
+    console.log(filterKnapper);
+    hentData(fil);
+
+}
+
+
+function filtrer() {
+    console.log("i work");
+    //sætter filters værdi lig med værdien fra data af den knap der førte ind i funktionen
+    filter = this.dataset.movie;
+
+    //fjerner og tilføjer valgt class til den rigtige knap
+    document.querySelector(".valgt").classList.remove("valgt");
+    this.classList.add("valgt");
+console.log(filter);
+
+    vis();
+}
 
 function vis() {
     let list = document.querySelector(".actorlist");
     let actortemplate = document.querySelector("template");
     console.log("ShowActors work");
 
- 
+    list.textContent = " ";
   actors.forEach(actor => {
-    const clone = actortemplate.cloneNode(true).content;
+    if (filter == actor.movie || filter == "all") {
+        const clone = actortemplate.cloneNode(true).content;
 
     clone.querySelector("li").textContent = actor.fullname;
     clone.querySelector("li").addEventListener("click", () => visDetails(actor));
 
     list.appendChild(clone);
+    }
   });
 }
+
 
 function visDetails (actor){
     popup.style.display="block";
@@ -35,5 +65,6 @@ function visDetails (actor){
     popup.querySelector("p").textContent = actor.movie;
     document.querySelector("#luk").addEventListener("click", () => popup.style.display="none");
 }
+
 
 
